3d8da0120afa7d69cef2017b55c207aba5241e64
khatabwedaa <khatabwedaa@gmail.com>
2020-05-04 19:08:58 +0200

    add highlight js

--- src/components/Alert/NotificationPop.vue
+++ src/components/Alert/NotificationPop.vue
@@ -26,15 +26,15 @@ export default {
         return {
             name: 'Notification Pop',
             code: `<div class="flex max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
-                        <div class="w-2 bg-gray-800"></div>
-                        <div class="flex items-center px-2 py-3">
-                            <img class="w-12 h-12 object-cover rounded-full" src="https://images.unsplash.com/photo-1477118476589-bff2c5c4cfbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60">
-                            <div class="mx-3">
-                                <h2 class="text-xl font-semibold text-gray-800">Hello john</h2>
-                                <p class="text-gray-600">Sara was replied on the <a href="#" class="text-blue-500">Upload Image</a>.</p>
-                            </div>
-                        </div>
-                    </div>`
+    <div class="w-2 bg-gray-800"></div>
+    <div class="flex items-center px-2 py-3">
+        <img class="w-12 h-12 object-cover rounded-full" src="https://images.unsplash.com/photo-1477118476589-bff2c5c4cfbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60">
+        <div class="mx-3">
+            <h2 class="text-xl font-semibold text-gray-800">Hello john</h2>
+            <p class="text-gray-600">Sara was replied on the <a href="#" class="text-blue-500">Upload Image</a>.</p>
+        </div>
+    </div>
+</div>`
         }
     }
 }
--- src/views/ViewComponet.vue
+++ src/views/ViewComponet.vue
@@ -12,10 +12,8 @@
                         </svg>
                     </button>
                     <button v-clipboard:copy="code" class="bg-gray-200 rounded-md p-1 text-gray-600 hover:text-gray-700 hover:bg-gray-400 focus:outline-none">
-                        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M8 11C7.44772 11 7 11.4477 7 12C7 12.5523 7.44772 13 8 13H15.9595C16.5118 13 16.9595 12.5523 16.9595 12C16.9595 11.4477 16.5118 11 15.9595 11H8Z"/>
-                            <path d="M8.04053 15.0665C7.48824 15.0665 7.04053 15.5142 7.04053 16.0665C7.04053 16.6188 7.48824 17.0665 8.04053 17.0665H16C16.5523 17.0665 17 16.6188 17 16.0665C17 15.5142 16.5523 15.0665 16 15.0665H8.04053Z"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M5 3C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H5ZM7 5H5L5 19H19V5H17V6C17 7.65685 15.6569 9 14 9H10C8.34315 9 7 7.65685 7 6V5ZM9 5V6C9 6.55228 9.44772 7 10 7H14C14.5523 7 15 6.55228 15 6V5H9Z"/>
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6">
+                            <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
                         </svg>
                     </button>
                </div>
@@ -28,7 +26,7 @@
             </div>
         </div>
 
-        <div v-if="viewCode" class="flex justify-center items-center p-6">
+        <div v-if="viewCode">
             <code-snippet :code="code"></code-snippet>
         </div>
     </div>    
