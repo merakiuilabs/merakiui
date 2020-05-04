5f058b26321228cc976b6c903578e1d211717fd2
khatabwedaa <khatabwedaa@gmail.com>
2020-05-04 14:59:42 +0200

    install vue-clipboard2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueClipboard from 'vue-clipboard2';
 
 Vue.config.productionTip = false
+
+Vue.use(VueClipboard);
 
 new Vue({
   render: h => h(App),
