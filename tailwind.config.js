dd7c125289819cbf0b91780e86e0fd1498d9fe5a
Mosab Ibrahim <miaababikir@gmail.com>
2020-05-05 03:16:59 +0200

    Adding purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {
-      fontFamily: {
-        inter: [
-          'Inter'
-        ],
-      },
-      spacing: {
-        '7': '1.75rem',
-        '9': '2.25rem',
-        '96': '24rem',
-      },
+    purge: [
+        './src/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: [
+                    'Inter'
+                ],
+            },
+            spacing: {
+                '7': '1.75rem',
+                '9': '2.25rem',
+                '96': '24rem',
+            },
+        },
     },
-  },
-  variants: {},
-  plugins: [],
+    variants: {},
+    plugins: [],
 }
