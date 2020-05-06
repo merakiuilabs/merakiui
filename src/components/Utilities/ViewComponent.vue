8c69f0ad4671707fce1bfe04e5aaebc723c7ae85
khatabwedaa <khatabwedaa@gmail.com>
2020-05-06 14:45:40 +0200

    edit copied pop time

--- src/components/Utilities/ViewComponent.vue
+++ src/components/Utilities/ViewComponent.vue
@@ -89,7 +89,7 @@
 
                 setTimeout(() => {
                     this.copy = false
-                }, 2000);
+                }, 800);
             },
         },
     }
@@ -99,7 +99,7 @@
     .fade-enter-active, .fade-leave-active {
         transition: opacity .5s;
     }
-    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    .fade-enter, .fade-leave-to {
         opacity: 0;
     }
 </style>
