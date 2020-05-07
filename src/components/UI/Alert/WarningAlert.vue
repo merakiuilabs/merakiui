ce5cf6e5782b35c1e5b2b9a39c0e0642809f855c
khatabwedaa <khatabwedaa@gmail.com>
2020-05-07 19:34:15 +0200

    Fix alert width

--- src/components/UI/Alert/WarningAlert.vue
+++ src/components/UI/Alert/WarningAlert.vue
@@ -1,6 +1,6 @@
 <template>
     <div :id="name | toId">
-        <div class="flex max-w-lg mx-auto bg-white rounded-lg overflow-hidden">
+        <div class="flex max-w-md mx-auto bg-white rounded-lg overflow-hidden">
             <div class="flex justify-center items-center w-16 py-3 bg-yellow-500">
                  <svg class="h-8 w-8 fill-current text-white" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
                     <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z"/>
