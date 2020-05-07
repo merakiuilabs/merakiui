b7ed396eef79ca79afdba5f0f9b976ccda038e89
Mosab Ibrahim <miaababikir@gmail.com>
2020-05-07 21:41:45 +0200

    Adding google analytic

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="min-h-screen bg-gray-100 font-roboto" id="app">
-    <Header></Header>
-    <master-component></master-component>
-  </div>
+    <div class="min-h-screen bg-gray-100 font-roboto" id="app">
+        <Header></Header>
+        <master-component></master-component>
+    </div>
 </template>
 
 <script>
-import Header from "./components/Header";
-import MasterComponent from "./components/Master";
+    import Header from "./components/Header";
+    import MasterComponent from "./components/Master";
 
-export default {
-  components: {
-    Header , MasterComponent
-  }
-}
+    export default {
+        components: {
+            Header, MasterComponent
+        },
+        mounted() {
+            this.$ga.page('/');
+        }
+    }
 </script>
 
 <style src="./assets/css/app.css"></style>
