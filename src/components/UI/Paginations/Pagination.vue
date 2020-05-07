021549886f3b70d27ff00ee0e8e35091b136a180
khatabwedaa <khatabwedaa@gmail.com>
2020-05-07 17:09:06 +0200

    make pagination center

--- src/components/UI/Paginations/Pagination.vue
+++ src/components/UI/Paginations/Pagination.vue
@@ -1,26 +1,28 @@
 <template>
-    <div :id="name | toId">
-        <ul class="flex mx-auto">
-            <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-500 rounded-lg">
-                <a class="flex items-center font-bold" href="#">
-                    <span class="mx-1">previous</span>
-                </a>
-            </li>
-            <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                <a class="font-bold" href="#">1</a>
-            </li>
-            <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                <a class="font-bold" href="#">2</a>
-            </li>
-            <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                <a class="font-bold" href="#">3</a>
-            </li>
-            <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                <a class="flex items-center font-bold" href="#">
-                    <span class="mx-1">Next</span>
-                </a>
-            </li>
-        </ul>
+    <div class="flex justify-center items-center">
+        <div :id="name | toId">
+            <ul class="flex">
+                <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-500 rounded-lg">
+                    <a class="flex items-center font-bold" href="#">
+                        <span class="mx-1">previous</span>
+                    </a>
+                </li>
+                <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
+                    <a class="font-bold" href="#">1</a>
+                </li>
+                <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
+                    <a class="font-bold" href="#">2</a>
+                </li>
+                <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
+                    <a class="font-bold" href="#">3</a>
+                </li>
+                <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
+                    <a class="flex items-center font-bold" href="#">
+                        <span class="mx-1">Next</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
