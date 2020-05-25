44606e2b7c73a9135e22d162f697e09c5613a025
khatabwedaa <khatabwedaa@gmail.com>
2020-05-25 17:29:39 +0200

    Update navbars style

--- src/components/UI/Navbars/NavbarWithSearch.vue
+++ src/components/UI/Navbars/NavbarWithSearch.vue
@@ -1,6 +1,6 @@
 <template>
     <div :id="name | toId">
-        <nav class="bg-white shadow-md">
+        <nav class="bg-white shadow">
             <div class="container mx-auto px-6 py-3 md:flex">
                 <div class="flex justify-between items-center">
                     <div>
@@ -8,7 +8,7 @@
                     </div>
 
                     <div class="md:hidden">
-                        <button @click="isOpen = !isOpen" type="button" class="block text-gray-800 hover:text-gray-700 focus:text-gray-700 focus:outline-none">
+                        <button @click="isOpen = !isOpen" type="button" class="text-gray-500 hover:text-gray-600 focus:text-gray-600 focus:outline-none">
                             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
                                 <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
                             </svg>
