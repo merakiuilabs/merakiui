43d4c5d7be119534d8f5f5b6f9bb1f72cef50330
Harsh Bharvada <harsh07bharvada@gmail.com>
2020-05-29 11:53:47 +0530

    Added svg arrows to Pagination

--- src/components/UI/Paginations/Pagination.vue
+++ src/components/UI/Paginations/Pagination.vue
@@ -4,7 +4,9 @@
             <ul class="flex">
                 <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-500 rounded-lg">
                     <a href="#" class="flex items-center font-bold cursor-not-allowed">
-                        <span class="mx-1">previous</span>
+                        <span class="mx-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#1a202c" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
+                        </span>
                     </a>
                 </li>
                 <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
@@ -18,7 +20,7 @@
                 </li>
                 <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
                     <a href="#" class="flex items-center font-bold">
-                        <span class="mx-1">Next</span>
+                        <span class="mx-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#1a202c" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></span>
                     </a>
                 </li>
             </ul>
