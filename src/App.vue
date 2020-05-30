20f8d8dbf85fed55cc0196c9d81fe97a70627c1b
khatabwedaa <khatabwedaa@gmail.com>
2020-05-30 23:07:57 +0200

    Change BG color

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
-    <div class="min-h-screen bg-gray-100 font-roboto" id="app">
+    <div class="min-h-screen bg-white font-roboto" id="app">
         <Header></Header>
+
         <main-component></main-component>
     </div>
 </template>
 
 <script>
-    import Header from "./components/Header";
-    import MainComponent from "./components/Main";
+import Header from "./components/Header";
+import MainComponent from "./components/Main";
 
-    export default {
-        components: {
-            Header, MainComponent
-        },
-        mounted() {
-            this.$ga.page('/');
-        }
+export default {
+    components: {
+        Header, MainComponent
+    },
+    mounted() {
+        this.$ga.page('/');
     }
+}
 </script>
 
 <style src="./assets/css/app.css"></style>
