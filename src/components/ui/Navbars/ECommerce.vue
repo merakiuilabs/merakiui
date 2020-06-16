05f99d7928545f8e2cf7845b3092d910fdb0ee50
khatabWedaa <38974561+khatabwedaa@users.noreply.github.com>
2020-06-16 11:14:06 +0200

    Update ECommerce.vue

--- src/components/ui/Navbars/ECommerce.vue
+++ src/components/ui/Navbars/ECommerce.vue
@@ -54,9 +54,10 @@
                     <div>
                         <a class="text-gray-800 text-xl font-bold md:text-2xl hover:text-gray-700" href="#">Brand</a>
                     </div>
-
+                    
+                    <!-- Mobile menu button -->
                     <div class="flex md:hidden">
-                        <button @click="isOpen = !isOpen" type="button" class="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
+                        <button type="button" class="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
                             <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
                                 <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
                             </svg>
@@ -64,7 +65,8 @@
                     </div>
                 </div>
 
-                <div :class="isOpen ? '' : 'hidden'" class="md:flex items-center">
+                <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+                <div class="md:flex items-center">
                     <div class="flex flex-col md:flex-row md:mx-6">
                         <a class="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0" href="#">Home</a>
                         <a class="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0" href="#">Shop</a>
