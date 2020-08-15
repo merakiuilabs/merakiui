d6ac8d2ed747aa7d0b90dc40fffab52abf274644
khatabwedaa <khatabwedaa@gmail.com>
2020-08-15 14:18:41 +0200

    Add OG image

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import App from './App.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
 import router from "./router";
-import VueClipboard from 'vue-clipboard2';
-import VueAnalytics from 'vue-analytics';
-import ViewComponent from './components/utilities/ViewComponent';
+import Meta from "vue-meta";
+import VueClipboard from "vue-clipboard2";
+import VueAnalytics from "vue-analytics";
+import ViewComponent from "./components/utilities/ViewComponent";
+
+Vue.use(Meta);
 
 Vue.use(VueRouter);
 
-Vue.component('view-component', ViewComponent);
+Vue.component("view-component", ViewComponent);
 
 Vue.config.productionTip = false;
 
 Vue.use(VueClipboard);
 
-Vue.use(VueAnalytics, { id: 'UA-167213766-1' });
+Vue.use(VueAnalytics, { id: "UA-167213766-1" });
 
-new Vue({ render: h => h(App), router }).$mount('#app');
+new Vue({ render: (h) => h(App), router }).$mount("#app");
