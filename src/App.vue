259caa118d996db7bdd3e606612e4dd10e1abd9b
khatabwedaa <khatabwedaa@gmail.com>
2020-08-23 10:50:11 +0200

    Create Components & ComponentsFilter

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,7 @@
 <template>
     <div class="bg-white font-roboto" id="app">
-        <main-header></main-header>
-
-        <main class="container mx-auto px-6" id="main">
-            <div class="mt-16 text-center">
-                <h1 class="text-xl md:text-3xl text-gray-800 font-medium">
-                    Discover new components. Build amazing things 🔥
-                </h1>
-            </div>
-
-            <div class="max-w-2xl mx-auto mt-4">
-                <!-- <div class="flex flex-wrap items-center justify-center">
-                    <span class="ml-2" v-for="route in routes" :key="route.path">
-                        <span v-if="route.path != '/'">
-                            <router-link class="inline-block mt-2 px-3 py-1 rounded text-sm cursor-pointer hover:bg-gray-700 hover:text-gray-200" :class="currentPage == route.path ? 'bg-gray-700 text-gray-200' : 'bg-gray-200 text-gray-700'" :to="route.path">
-                                {{ route.name }}
-                            </router-link>
-                        </span>
-                    </span>
-                </div> -->
-
-                <p class="mt-8 text-gray-700 text-center">
-                    Heros components require simple <span class="font-semibold">- don't panic please 😎 -</span>
-                    <a href="https://gist.github.com/Miaababikir/052e31b345781c0f73180b80a285781b" target="_blank" class="text-blue-600 hover:underline"> configuration</a>
-                </p>
-            </div>
-
-            <div class="mt-16">
-                <error-full-width></error-full-width>
-            </div>
-        </main>
+        <header-component></header-component>
+        <main-component></main-component>
 
         <back-to-top visibleoffset="800">
             <button class="bg-gray-800 text-white fixed bottom-0 right-0 my-10 mx-10 p-2 rounded-md focus:outline-none" aria-label="Back to top">
@@ -42,18 +14,12 @@
 </template>
 
 <script>
-import MainHeader from "./components/Header";
+import HeaderComponent from "./components/Header";
+import MainComponent from "./components/Main";
 import BackToTop from "vue-backtotop";
 
-import ErrorFullWidth from "./components/ui/Alerts/ErrorFullWidth";
-
 export default {
-    components: {
-        MainHeader,
-        BackToTop,
-
-        ErrorFullWidth
-    },
+    components: { HeaderComponent, MainComponent, BackToTop },
 
     data() {
         return {
---
+++ src/models/Components.js
@@ -0,0 +1,84 @@
+export default [
+    {
+        name: 'Alerts',
+        components: [
+            { name: 'SuccessPop' },
+            { name: 'InfoPop' },
+            { name: 'WarningPop' },
+            { name: 'ErrorPop' },
+            { name: 'NotificationPop' },
+            { name: 'SuccessFullWidth' },
+            { name: 'InfoFullWidth' },
+            { name: 'WarningFullWidth' },
+            { name: 'ErrorFullWidth' },
+
+        ],
+    },
+    {
+        name: 'Authentication',
+        components: [
+            { name: 'SimpleLogin' },
+            { name: 'LoginWithSideImage' },
+        ]
+    },
+    {
+        name: 'Cards',
+        components: [
+            { name: 'Article' },
+            { name: 'ArticleWithSMIcons' },
+            { name: 'ArticleWithImage' },
+            { name: 'Product' },
+            { name: 'ProductWithEvaluation' },
+            { name: 'SimpleProduct' },
+            { name: 'Testimonial' },
+            { name: 'User' },
+            { name: 'UserWithDetails' },
+        ],
+    },
+    {
+        name: 'Forms',
+        components: [
+            { name: 'Simple' },
+            { name: 'Newsletter' },
+        ],
+    },
+    {
+        name: 'Navbars',
+        components: [
+            { name: 'ECommerce' },
+            { name: 'WithSearch' },
+            { name: 'WithAvatar' },
+        ]
+    },
+    {
+        name: 'Heros',
+        components: [
+            { name: 'ECommerce' },
+            { name: 'WithImage' },
+            { name: 'WithPattern' },
+        ],
+    },
+    {
+        name: 'Sections',
+        components: [
+            { name: 'AboutMe' },
+            { name: 'Feature' },
+            { name: 'OurTeam' },
+            { name: 'ParagraphWithImage' },
+        ]
+    },
+    {
+        name: 'Paginations',
+        components: [
+            { name: 'Simple' },
+        ]
+    },
+    {
+        name: 'Footers',
+        components: [
+            { name: 'Simple' },
+            { name: 'WithDetails' },
+            { name: 'WithSubscribeForm' },
+        ]
+    },
+]
