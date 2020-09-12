0a14712f993d2c000e48a455c6da71fed591c9d1
khatabwedaa <khatabwedaa@gmail.com>
2020-09-12 16:28:37 +0200

    Update simple pagination

--- src/components/ui/Paginations/Simple.vue
+++ src/components/ui/Paginations/Simple.vue
@@ -2,31 +2,27 @@
     <view-component :name="name" :code="code">
         <div class="px-3 py-6" slot="component">
             <div class="flex justify-center items-center">
-                <ul class="flex">
-                    <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-500 rounded-lg">
-                        <a href="#" class="flex items-center font-bold cursor-not-allowed">
-                            <span class="mx-1">previous</span>
-                        </a>
-                    </li>
+                <div class="flex">
+                    <a href="#" class="md:mx-1 px-3 py-2 bg-white text-gray-500 rounded-md cursor-not-allowed">
+                        previous
+                    </a>
 
-                    <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                        <a href="#" class="font-bold">1</a>
-                    </li>
+                    <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 hover:bg-indigo-600 hover:text-white rounded-md">
+                        1
+                    </a>
 
-                    <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                        <a href="#" class="font-bold">2</a>
-                    </li>
+                    <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 hover:bg-indigo-600 hover:text-white rounded-md">
+                        2
+                    </a>
 
-                    <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                        <a href="#" class="font-bold">3</a>
-                    </li>
+                    <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 hover:bg-indigo-600 hover:text-white rounded-md">
+                        3
+                    </a>
 
-                    <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                        <a href="#" class="flex items-center font-bold">
-                            <span class="mx-1">Next</span>
-                        </a>
-                    </li>
-                </ul>
+                    <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 hover:bg-indigo-600 hover:text-white rounded-md">
+                        Next
+                    </a>
+                </div>
             </div>
         </div>
     </view-component>
@@ -38,31 +34,27 @@
             return {
                 name: 'Pagination',
                 code: `
-        <ul class="flex">
-            <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-500 rounded-lg">
-                <a href="#" class="flex items-center font-bold cursor-not-allowed">
-                    <span class="mx-1">previous</span>
-                </a>
-            </li>
+        <div class="flex">
+            <a href="#" class="md:mx-1 px-3 py-2 bg-white text-gray-500 rounded-md cursor-not-allowed">
+                previous
+            </a>
 
-            <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                <a href="#" class="font-bold">1</a>
-            </li>
+            <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 hover:bg-indigo-600 hover:text-white rounded-md">
+                1
+            </a>
 
-            <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                <a href="#" class="font-bold">2</a>
-            </li>
+            <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 hover:bg-indigo-600 hover:text-white rounded-md">
+                2
+            </a>
 
-            <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                <a href="#" class="font-bold">3</a>
-            </li>
-            
-            <li class="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-                <a href="#" class="flex items-center font-bold">
-                    <span class="mx-1">Next</span>
-                </a>
-            </li>
-        </ul>`
+            <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 hover:bg-indigo-600 hover:text-white rounded-md">
+                3
+            </a>
+
+            <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 hover:bg-indigo-600 hover:text-white rounded-md">
+                Next
+            </a>
+        </div>`
             }
         }
     }
