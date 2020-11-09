131fd49be2a955436167ea7c79526712c49d4f84
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2020-11-09 20:42:15 +0100

    💄 Use AppButton on scroll-top button

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,23 @@
         <main-component></main-component>
 
         <back-to-top visibleoffset="800">
-            <button class="bg-gray-800 text-white fixed bottom-0 right-0 my-10 mx-10 p-2 rounded-md focus:outline-none" aria-label="Back to top">
+            <app-button class="bg-gray-800 hover:bg-gray-900 text-white fixed bottom-0 right-0 my-10 mx-10 p-2 rounded-md focus:outline-none" aria-label="Back to top">
                 <svg class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd"/>
                 </svg>
-            </button>
+            </app-button>
         </back-to-top>
     </div>
 </template>
 
 <script>
+import AppButton from "@/components/utilities/AppButton.vue";
 import HeaderComponent from "./components/Header";
 import MainComponent from "./components/Main";
 import BackToTop from "vue-backtotop";
 
 export default {
-    components: { HeaderComponent, MainComponent, BackToTop },
+    components: { AppButton, HeaderComponent, MainComponent, BackToTop },
 
     data() {
         return {
