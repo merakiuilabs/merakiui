1c757ff11887f13f495a239cb13f7f382d77d0be
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2020-11-09 20:41:05 +0100

    ✨ AppButton to abstract transition logic

---
+++ src/components/utilities/AppButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    v-bind="$attrs"
+    v-on="$listeners"
+    class="motion-reduce:transition-none transition-colors duration-150 ease-in-out"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'AppButton'
+}
+</script>
