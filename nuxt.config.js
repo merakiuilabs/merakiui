479cf3e5e85fd2604cbb04c5212cbcdfb00aa1f0
khatabwedaa <khatabwedaa@gmail.com>
2021-04-09 22:49:05 +0200

    🤠

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,12 @@ export default {
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
