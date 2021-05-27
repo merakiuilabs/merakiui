fb8a4546afd8858cb8ad85861e41faadb79cca7b
khatabwedaa <khatabwedaa@gmail.com>
2021-05-27 21:02:09 +0200

    Upgrade postcss to v8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
     purge: [
         './components/**/*.{vue,js}',
-        './layouts/**/*.vue',
         './pages/**/*.vue',
+        './plugins/**/*.{js,ts}',
         './nuxt.config.{js,ts}',
     ],
+    mode: 'jit',
     darkMode: "class",
     theme: {
         extend: {
