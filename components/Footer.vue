d3c914f7fa12ab3b7ce7ab4e64960f9a9b36e4e6
khatabwedaa <khatabwedaa@gmail.com>
2021-05-27 20:42:07 +0200

    Add Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container px-6 mx-auto">
-        <div class="flex flex-col items-center border-t border-gray-200">
+        <div class="flex flex-col items-center border-t border-gray-200 dark:border-gray-800">
             <div class="py-6 text-center">
-                <p class="text-sm text-gray-600">© {{ (new Date().getFullYear()) }} by Baka Team</p>
+                <p class="text-gray-500 dark:text-gray-300">© <a href="https://github.com/bakateam" target="_blank" class="text-blue-500 hover:underline">Baka Team</a> {{ (new Date().getFullYear()) }} - All rights reserved</p>
             </div>
         </div>
     </div>
