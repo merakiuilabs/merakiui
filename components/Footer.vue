d6757a79189e6f81c3c03183fabdfa425cd156b7
khatabwedaa <khatabwedaa@gmail.com>
2021-06-29 15:46:43 +0200

    Update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,10 @@
     <div class="container px-6 mx-auto">
         <div class="flex flex-col items-center border-t border-gray-200 dark:border-gray-800">
             <div class="py-6 text-center">
-                <p class="text-gray-500 dark:text-gray-300">© <a href="https://github.com/bakateam" target="_blank" class="text-blue-500 hover:underline">Baka Team</a> {{ (new Date().getFullYear()) }} - All rights reserved</p>
+                <p class="text-gray-500 dark:text-gray-300">Build with ✨ BY 
+                    <a href="https://twitter.com/khatabwedaa" target="_blank" class="text-blue-500 hover:underline">Khatab Wedaa</a> &
+                    <a href="https://twitter.com/miaababikir" target="_blank" class="text-blue-500 hover:underline">Mosab Ibrahim</a>
+                </p>
             </div>
         </div>
     </div>
