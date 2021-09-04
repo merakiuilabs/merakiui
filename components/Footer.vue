d5760933152106aba1903e48b1a4bf4d8054e1f6
khatabwedaa <khatabwedaa@gmail.com>
2021-09-04 23:00:27 +0200

    fix play link button

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container px-6 mx-auto">
-        <div class="flex flex-col items-center border-t border-gray-200 dark:border-gray-800">
+        <div class="flex flex-col items-center">
             <div class="py-6 text-center">
                 <p class="text-gray-500 dark:text-gray-300">Build with ✨ BY 
                     <a href="https://twitter.com/khatabwedaa" target="_blank" class="text-blue-500 hover:underline">Khatab Wedaa</a> &
--- components/utilities/ViewComponent.vue
+++ components/utilities/ViewComponent.vue
@@ -28,7 +28,7 @@
                     </svg>
                 </app-button>
 
-                <a target="_blank" :href="playUrl" class="block p-1 text-gray-600 bg-gray-200 rounded-md dark:bg-gray-500 dark:text-gray-200 hover:text-gray-700 hover:bg-gray-400 dark:hover:bg-gray-500 dark:hover:text-gray-200 focus:outline-none">
+                <a target="_blank" :href="playUrl" class="block p-1 text-gray-600 bg-gray-200 rounded-md dark:bg-gray-700 dark:hover:bg-gray-500 dark:text-gray-200 hover:text-gray-700 hover:bg-gray-400 dark:hover:text-gray-200 focus:outline-none">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                     </svg>
