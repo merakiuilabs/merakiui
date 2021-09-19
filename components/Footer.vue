2ffe742ebbbbc9329a7cffea1bc0718bba7a14fa
khatabwedaa <khatabwedaa@gmail.com>
2021-09-20 00:04:36 +0200

    Drop website dark mode feature

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
     <div class="container px-6 mx-auto">
         <div class="flex flex-col items-center">
             <div class="py-6 text-center">
-                <p class="flex items-center text-sm text-gray-500 sm:text-base dark:text-gray-300">
+                <p class="flex items-center text-sm text-gray-500 sm:text-base">
                     Build with 
                     <svg class="w-5 h-5 mx-1 text-gray-700" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M2 8.40001C1.99975 6.95035 2.58239 5.56146 3.61681 4.54584C4.65124 3.53022 6.05058 2.97317 7.5 3.00001C9.21732 2.99089 10.856 3.71919 12 5.00001C13.144 3.71919 14.7827 2.99089 16.5 3.00001C17.9494 2.97317 19.3488 3.53022 20.3832 4.54584C21.4176 5.56146 22.0002 6.95035 22 8.40001C22 13.756 15.621 17.8 12 21C8.387 17.773 2 13.76 2 8.40001Z" fill="currentColor"></path>
--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@
                     </div>
                 </div>
 
-                <div class="hidden w-full mt-6 md:flex md:items-center md:justify-center lg:w-1/2 lg:mt-0">
+                <div class="flex items-center justify-center w-full mt-6 lg:w-1/2 lg:mt-0">
                     <div class="w-full animate-float">
                         <img src="../assets/images/header-img.png" alt="moon">
                     </div>
@@ -48,7 +48,7 @@
     <div class="container px-4 mx-auto">
         <div class="py-16 text-center">
             <h1
-                class="text-xl font-medium text-gray-700 sm:flex sm:items-center sm:justify-center lg:text-3xl dark:text-white"
+                class="text-xl font-medium text-gray-700 sm:flex sm:items-center sm:justify-center lg:text-3xl"
             >
                 <span>Discover new components. Build amazing things. </span> &nbsp;
                 
@@ -66,11 +66,11 @@
                         >
                             <app-button
                                 @click="activeCategory = category.name"
-                                class="px-3 py-1 mt-2 ml-2 text-sm rounded cursor-pointer hover:bg-gray-700 hover:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-gray-200 focus:outline-none"
+                                class="px-3 py-1 mt-2 ml-2 text-sm rounded cursor-pointer hover:bg-gray-700 hover:text-gray-200 focus:outline-none"
                                 :class="
                                     category.name === activeCategory
-                                        ? 'bg-gray-700 text-gray-200 dark:bg-gray-600 dark:text-gray-200'
-                                        : 'bg-gray-200 text-gray-700 dark:bg-gray-800 dark:text-gray-200'"
+                                        ? 'bg-gray-700 text-gray-200'
+                                        : 'bg-gray-200 text-gray-700'"
                                 >
                                 {{ category.name }}
                             </app-button>
@@ -87,7 +87,7 @@
         <div class="w-full max-w-6xl mx-auto">
             <div class="mb-16" v-for="category in list" :key="category.name">
                 <h1
-                    class="mb-6 text-2xl font-semibold text-gray-800 capitalize dark:text-white"
+                    class="mb-6 text-2xl font-semibold text-gray-800 capitalize"
                     v-text="category.name"
                 ></h1>
 
