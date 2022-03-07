7c955a3c650dea3e151c9a99f0815e8ea12f95ad
Arslan Jan Soomro <arslanjs.dev@gmail.com>
2022-03-07 00:30:01 -0800

    Add Frame and Resizer Files

---
+++ components/utilities/Frame.vue
@@ -0,0 +1,56 @@
+<template>
+    <iframe class="area-full" frameborder="0" width="100%" height="100%" :srcdoc="doc" ></iframe>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                code: `<div class="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div class="flex items-center justify-center w-12 bg-emerald-500">
+            <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z"/>
+            </svg>
+        </div>
+        
+        <div class="px-4 py-2 -mx-3">
+            <div class="mx-3">
+                <span class="font-semibold text-emerald-500 dark:text-emerald-400">Success</span>
+                <p class="text-sm text-gray-600 dark:text-gray-200">Your account was registered!</p>
+            </div>
+        </div>
+    </div>`
+            }
+        },
+
+        computed: {
+            doc: function () {
+                return `<!DOCTYPE html>
+                            <html class="w-full h-full" lang="en">
+                            <head>
+                                <meta charset="UTF-8">
+                                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <title>Component</title>
+                                <script src="https://cdn.tailwindcss.com"><\/script>
+                            </head>
+                            <body class="w-full h-full flex justify-center items-center">
+                                ${this.code}
+                            </body>
+                        </html>`;
+            }
+        }
+    }
+
+
+</script>
+
+<style>
+
+    .area-full{
+        width: 100%;
+        height: 100%;
+        border: 1px solid #d1d1d1;
+    }
+
+</style>
---
+++ components/utilities/Resizer.vue
@@ -0,0 +1,154 @@
+<template>
+  <div
+    class="cont"
+    @mousemove="paneMouseMoveHandler"
+    @mouseup="paneMouseUpHandler"
+    @mouseleave="paneMouseUpHandler"
+    ref="cont"
+    style="{max-width: 100px}"
+  >
+    <div class="pane" :style="stylesObj">
+      <slot class="content"></slot>
+      <div
+        class="handle"
+        ref="handle"
+        @mousedown="handleMouseDownHandler"
+      >
+        <img src="../assets/equal-sign.png">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+//CONSTRAINT, that the pane would always be the width of container that means it will take 100% of space initially.
+
+export default {
+
+    data () {
+        return {
+            mouseReferPoint : undefined, //Refers to the point where mouse was clicked down
+            paneWidth : undefined, //The variable width of the pane that the user can resize
+            stylesObj : { }, //The styles object to mainly control width of the pane
+            resizeEndTimer : undefined, //Used to calculate the end of the resize event
+        }
+    },
+
+  mounted() {
+    const totalWidth = this.$refs.cont.clientWidth;
+    this.paneWidth = totalWidth;
+    window.addEventListener('resize', this.wResizeHandler);
+  },
+
+  beforeDestroy() {
+    window.removeEventListener('resize', this.wResizeHandler);
+  },
+
+  watch : {
+    paneWidth : function () {
+      if(this.paneWidth != undefined){
+        this.stylesObj.width = this.paneWidth + 'px';
+      }
+    }
+  },
+
+  methods: {
+
+      handleMouseDownHandler : function (e) {
+          this.mouseReferPoint = e.x;
+      },
+
+      paneMouseMoveHandler : function (e) {
+          const maxWidth = this.$refs.cont.offsetWidth;
+          const minWidth = 100;
+          let moveRatio = undefined;
+
+          if(this.mouseReferPoint != undefined ){
+
+            //if e.x < mouseReferPoint then mouse is moving left otherwise right
+            moveRatio = e.x - this.mouseReferPoint;
+                if(this.paneWidth + moveRatio >= maxWidth){
+                    this.paneWidth = maxWidth;  
+                }else if(this.paneWidth + moveRatio <= minWidth){
+                    this.paneWidth = minWidth;
+                }else{
+                    this.paneWidth += moveRatio;
+                }
+
+              //Reset Refer point to recent crossed point
+              this.mouseReferPoint = e.x;
+          }
+      },
+
+      paneMouseUpHandler : function (e) {
+          this.mouseReferPoint = undefined;
+      },
+
+      wResizeHandler : function () {
+        //clientWidth -> width without border, offsetWidth -> width with borders included
+
+        const contWidth = this.$refs.cont?.clientWidth;
+        if(contWidth != undefined){
+          // +5 to still resize the pane if it's still in container's vicinity
+          if((this.paneWidth+5) >= contWidth){
+            this.stylesObj.width = '100%';
+            if(this.resizeEndTimer != undefined) clearTimeout(this.resizeEndTimer);
+            this.resizeEndTimer = setTimeout(this.wResizeEndHandler , 200);
+          }; 
+      }
+    },
+
+    wResizeEndHandler : function () {
+      const contWidth = this.$refs.cont?.clientWidth;
+      if(contWidth != undefined){
+        this.paneWidth = contWidth;
+      }
+    }
+
+  },
+
+};
+</script>
+
+<style>
+.cont {
+  height: 100%;
+  width: 100%;
+  max-width: 100%;
+  overflow: hidden;
+}
+
+.pane {
+  height: 100%;
+  width: 100%;
+  max-width: 100%;
+  position: relative;
+}
+
+.content {
+  z-index: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.handle {
+  width: 30px;
+  height: 100%;
+  background: #dedede;
+  cursor: col-resize;
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 10;
+
+  /*For Handling icon inside*/
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.handle img{
+  width: 20px;
+  height: 20px;
+}
+</style>
