dd08bab1cad5cab2568000efa137aaf17dcec568
khatabwedaa <khatabwedaa@gmail.com>
2022-08-14 12:10:07 +0200

    Rebranding banner

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="relative bg-white font-roboto">
+        <section class="flex items-center justify-center px-6 py-4 bg-gray-800 ">
+            <span class="text-lg tracking-wide text-white">Meraki UI Rebranding on <span class="underline decoration-blue-400">26 AUG</span> stay tuned</span>
+        </section>
         <Nuxt />
     </div>
 </template>
